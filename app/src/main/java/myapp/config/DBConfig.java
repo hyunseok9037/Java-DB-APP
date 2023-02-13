f10d270ca071ce90d2435b1a8f987c35b56036ea
hyunseok <dopocoo@naver.com>
2023-02-13 16:51:54 +0900

    자바 DB APP

---
+++ app/src/main/java/myapp/config/DBConfig.java
@@ -0,0 +1,21 @@
+package myapp.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConfig {
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/metadb", "root", "1234");
+            System.out.println("DB연결성공");
+            return conn;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+}
